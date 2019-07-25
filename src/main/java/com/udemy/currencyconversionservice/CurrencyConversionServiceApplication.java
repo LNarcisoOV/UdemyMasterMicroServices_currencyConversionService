cf7183522907a964ff41ba2b1dcf3bff8b0987bf
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-25 15:19:43 -0300

    Enabling FEIGN to the project.

--- src/main/java/com/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,11 @@ package com.udemy.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//Package for the Spring Boot's version 2.0.0.M4.
+@EnableFeignClients("org.springframework.cloud.openfeign.*")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
