0cf739b52550738ba376ef20f4e6c3ef879a35a8
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-25 17:23:52 -0300

    Configuring Ribbon to know the list of servers or instances of currencyExchangeService.

--- src/main/java/com/udemy/currencyconversionservice/repository/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/udemy/currencyconversionservice/repository/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.udemy.currencyconversionservice.repository;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,10 +10,15 @@ import com.udemy.currencyconversionservice.model.CurrencyConversion;
 //NAME should be the value in the property 'spring.application.name' in the 
 //application.properties file of the microservice wich will be consumed.
 //URL should be the host and the port of the microservice.
-@FeignClient(name = "currencyExchangeService", url = "http://localhost:8000/currencyExchangeService")
+//@FeignClient(name = "currencyExchangeService", url = "http://localhost:8000/currencyExchangeService")
+
+//With RibbonClient annotation, we don't need to inform the URL in FeignClient annotation,
+//but is necessary configure in application.properties file.
+@FeignClient(name = "currencyExchangeService")
+@RibbonClient(name = "currencyExchangeService")
 public interface CurrencyExchangeServiceProxy {
 
-	//Method whitch will be consumed in currency exchange microservice.
+	// Method whitch will be consumed in currency exchange microservice.
 	@GetMapping("/from/{fromCurrency}/to/{toCurrency}")
 	public CurrencyConversion retrieveExchangeValue(@PathVariable("fromCurrency") String fromCurrency,
 			@PathVariable("toCurrency") String toCurrency);
