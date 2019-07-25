01bd4635978bda2fdf271198d133eea436bfbd49
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-25 15:14:39 -0300

    Adding spring cloud feign dependency to the project.

--- src/main/java/com/udemy/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/udemy/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,9 +18,6 @@ public class CurrencyConversionController {
 
 	private static final String CURRENCY_EXCHANGE_SERVICE_URI = "http://localhost:8000/currencyExchangeService/from/{fromCurrency}/to/{toCurrency}/";
 
-	@Autowired
-	private Environment environment;
-
 	@GetMapping("/from/{fromCurrency}/to/{toCurrency}/quantity/{quantity}")
 	public CurrencyConversion retrieveExchangeValue(@PathVariable String fromCurrency,
 			@PathVariable String toCurrency, @PathVariable BigDecimal quantity) {
@@ -34,11 +29,12 @@ public class CurrencyConversionController {
 		try {
 			ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
 					.getForEntity(CURRENCY_EXCHANGE_SERVICE_URI, CurrencyConversion.class, uriVariables);
+
 			CurrencyConversion response = responseEntity.getBody();
+
 			return new CurrencyConversion(response.getId(), fromCurrency, toCurrency,
 					response.getConservionMultiple(), quantity,
-					quantity.multiply(response.getConservionMultiple()),
-					response.getPort());
+					quantity.multiply(response.getConservionMultiple()), response.getPort());
 		} catch (RestClientException r) {
 			throw new RestClientException("Currency exchange service not found.");
 		}
