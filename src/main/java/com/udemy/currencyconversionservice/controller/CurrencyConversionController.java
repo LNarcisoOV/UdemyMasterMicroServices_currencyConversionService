e694646ef106dac8832dc4b5111cbbccbe5df12e
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-25 14:45:03 -0300

    Consuming currency exchange service.

--- src/main/java/com/udemy/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/udemy/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,17 +1,24 @@
 package com.udemy.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemy.currencyconversionservice.model.CurrencyConversion;
 
 @RestController
 public class CurrencyConversionController {
+
+	private static final String CURRENCY_EXCHANGE_SERVICE_URI = "http://localhost:8000/currencyExchangeService/from/{fromCurrency}/to/{toCurrency}/";
 
 	@Autowired
 	private Environment environment;
@@ -20,14 +27,20 @@ public class CurrencyConversionController {
 	public CurrencyConversion retrieveExchangeValue(@PathVariable String fromCurrency,
 			@PathVariable String toCurrency, @PathVariable BigDecimal quantity) {
 
-		CurrencyConversion concurrentConversion = new CurrencyConversion(1000L, fromCurrency, toCurrency,
-				BigDecimal.ONE, quantity, quantity, 0);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("fromCurrency", fromCurrency);
+		uriVariables.put("toCurrency", toCurrency);
 
-		// if (exchangeValue == null) {
-		// throw new ExchangeValueNotFoundException("Exchange value not found");
-		// }
-
-		concurrentConversion.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
-		return concurrentConversion;
+		try {
+			ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+					.getForEntity(CURRENCY_EXCHANGE_SERVICE_URI, CurrencyConversion.class, uriVariables);
+			CurrencyConversion response = responseEntity.getBody();
+			return new CurrencyConversion(response.getId(), fromCurrency, toCurrency,
+					response.getConservionMultiple(), quantity,
+					quantity.multiply(response.getConservionMultiple()),
+					Integer.parseInt(environment.getProperty("local.server.port")));
+		} catch (RestClientException r) {
+			throw new RestClientException("Currency exchange service not found.");
+		}
 	}
 }
