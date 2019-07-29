57b0a89e3dce005478bc6022f7b6f1ce3e3c16af
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-29 10:39:57 -0300

    Enabling Eureka.

--- src/main/java/com/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.udemy.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 //Package for the Spring Boot's version 2.0.0.M4.
 @EnableFeignClients(basePackages = {"org.springframework.cloud.openfeign.*"
 		, "com.udemy.currencyconversionservice.repository"})
